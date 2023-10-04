669752e9b995ef204c49571a3369a0bb6a0a7158
miyehn <rainduym@gmail.com>
2023-10-04 05:11:10 -0700

    export part

--- src/EchoesExporter/AssetPack.cpp
+++ src/EchoesExporter/AssetPack.cpp
@@ -1,28 +1,63 @@
 #include <Windows.h>
+#include "stb_image_resize.h"
 #include "stb_image_write.h"
 #include "AssetPack.h"
 #include "Log.h"
 
-void TestWritePng(const std::string& outDir) {
-	std::vector<uint8_t> test_data;
-	for (int y = 0; y < 256; y++) {
-		for (int x = 0; x < 256; x++) {
-			test_data.push_back(50);
-			test_data.push_back(100);
-			test_data.push_back(200);
-			test_data.push_back(255);
-		}
-	}
+void WritePngToDirectory(
+	const std::vector<uint8_t>& data,
+	uint32_t width, uint32_t height,
+	const std::string& outDir,
+	const std::string& fileName,
+	float resizeRatio = 1.0f
+) {
 	// from: https://stackoverflow.com/questions/9235679/create-a-directory-if-it-doesnt-exist
 	if (CreateDirectory(outDir.c_str(), NULL) || ERROR_ALREADY_EXISTS == GetLastError()) {
-		std::string fullpath = outDir + "/test.png";
-		EXPECT(stbi_write_png(fullpath.c_str(), 256, 256, 4, test_data.data(), 4 * 256) != 0, true)
+		std::string fullpath = outDir + "/" + fileName;
+
+		// resize
+		uint32_t newWidth = std::ceil(width * resizeRatio);
+		uint32_t newHeight = std::ceil(height * resizeRatio);
+		std::vector<uint8_t> resizedData(newWidth * newHeight * 4);
+		stbir_resize_uint8(
+			data.data(), width, height, width * 4,
+			resizedData.data(), newWidth, newHeight, newWidth * 4, 4);
+
+		// write
+		EXPECT(stbi_write_png(fullpath.c_str(), newWidth, newHeight, 4, resizedData.data(), 4 * newWidth) != 0, true)
 	} else {
 		ERR("failed to create dir")
 	}
 }
 
+std::vector<uint8_t> Crop(const std::vector<uint8_t>& data, uint32_t srcStrideInBytes, ivec2 minPx, ivec2 sizePx) {
+	std::vector<uint8_t> result(sizePx.x * sizePx.y * 4);
+	uint32_t dstStrideInBytes = sizePx.x * 4;
+	for (int i = 0; i < sizePx.y; i++) {
+		uint32_t srcStart = (minPx.y + i) * srcStrideInBytes + minPx.x * 4;
+		uint32_t dstStart = i * dstStrideInBytes;
+		memcpy(result.data() + dstStart, data.data() + srcStart, dstStrideInBytes);
+	}
+	return result;
+}
+
 bool ExportAssetPack(const AssetPack& assetPack, const std::string& outDir) {
-	TestWritePng(outDir);
+
+	// compute resize ratio
+	const float standardPPDU = std::sqrt(2.0f) * 100;
+	float resizeRatio = standardPPDU / assetPack.pixelsPerDiagonalUnit;
+	LOG("PPDU: %.3f, resize ratio: %.3f", assetPack.pixelsPerDiagonalUnit, resizeRatio)
+
+	for (auto& pair : assetPack.spriteSets) {
+		const SpriteSet& sprite = pair.second;
+		auto baseRegionData = Crop(sprite.baseLayerData, assetPack.docWidth * 4, sprite.minPx, sprite.sizePx);
+		WritePngToDirectory(baseRegionData, sprite.sizePx.x, sprite.sizePx.y, outDir, sprite.name+"_base.png", resizeRatio);
+		for (int i = 0; i < sprite.lightLayersData.size(); i++) {
+			auto lightTexRegionData = Crop(sprite.lightLayersData[i], assetPack.docWidth * 4, sprite.minPx, sprite.sizePx);
+			WritePngToDirectory(
+				lightTexRegionData, sprite.sizePx.x, sprite.sizePx.y, outDir,
+				sprite.name+"_L" + std::to_string(i) +".png", resizeRatio);
+		}
+	}
 	return true;
 }
